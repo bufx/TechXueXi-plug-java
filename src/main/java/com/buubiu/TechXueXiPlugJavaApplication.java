a1379c56b0970709dd54c9a928757abff3123ef1
bufx <bufanxiang@gmail.com>
2021-11-29 16:08:57 +0800

    初始化

---
+++ src/main/java/com/buubiu/TechXueXiPlugJavaApplication.java
@@ -0,0 +1,13 @@
+package com.buubiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechXueXiPlugJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechXueXiPlugJavaApplication.class, args);
+	}
+
+}
